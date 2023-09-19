c41d7d02d7e623ab7e2307f5f3a3f70718508552
Galitsky Dmytro <melles.android@gmail.com>
2023-09-19 21:31:42 +0300

    Code Optimization
    1. **Optimizing the ContainsRussianCharacters Function**:
       Using regular expressions to check for Russian characters in the text is a great idea. This will make the code more understandable and faster.
    
    2. **Reducing Redundant API Calls to WoW**:
       Optimizing API calls to check player names in the group is an important step. Instead of multiple individual calls, you are using a single GetRaidRosterInfo call to retrieve information about all players, which will improve performance and avoid unnecessary load on the game.

--- core/core.lua
+++ core/core.lua
@@ -12,29 +12,26 @@ end
 
 -- Функція, яка перевіряє імена гравців у групі
 function addon:CheckGroupMembers()
-    if IsInGroup() or IsInRaid() then
-        -- Очищуємо список перед перевіркою
-        wipe(russianPlayerNames)
-        local numMembers = GetNumGroupMembers()
-        for i = 1, numMembers do
-            local name, _, _, _, _, _, _, _, _, _, _ = GetRaidRosterInfo(i)
-            if name and ContainsRussianCharacters(name) then
-                -- Зберігаємо ім'я гравця в таблиці, якщо його ще немає
-                if not tContains(russianPlayerNames, name) then
-                    table.insert(russianPlayerNames, name)
-                end
+    if not IsInGroup() and not IsInRaid() then
+        return
+    end
+
+    wipe(russianPlayerNames)
+    local numMembers = GetNumGroupMembers()
+    for i = 1, numMembers do
+        local name, _, _, _, _, _, _, _, _, _, _ = GetRaidRosterInfo(i)
+        if name and ContainsRussianCharacters(name) then
+            if not tContains(russianPlayerNames, name) then
+                table.insert(russianPlayerNames, name)
             end
         end
     end
 
-    -- Перевіряємо, чи є російські гравці в групі та виводимо їх імена у повідомленні
     if #russianPlayerNames > 0 then
-        local playerList = table.concat(russianPlayerNames, "\n") -- Кожне ім'я в новому рядку
-        -- Завантажуємо аддон Dialogs, якщо він ще не завантажений
+        local playerList = table.concat(russianPlayerNames, "\n")
         if not IsAddOnLoaded("RussianNameChecker_Dialogs") then
             LoadAddOn("RussianNameChecker_Dialogs")
         end
-        -- Викликаємо функцію з Dialogs.lua, передаючи список імен
         WarnRussianPlayersDetected(playerList)
         GroupUtils_LeaveGroup(playerList)
     end
@@ -42,16 +39,5 @@ end
 
 -- Функція, яка перевіряє, чи містить рядок російські букви
 function ContainsRussianCharacters(text)
-    -- Визначте російські букви або символи, які ви хочете перевірити
-    local russianCharacters = { "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н",
-        "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я" }
-
-    -- Перевірте, чи містить рядок російські букви
-    for _, character in ipairs(russianCharacters) do
-        if string.find(text, character) then
-            return true
-        end
-    end
-
-    return false
+    return string.match(text, "[а-яА-ЯёЁ]")
 end
